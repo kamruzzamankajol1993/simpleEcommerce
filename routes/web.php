7814c064e0d76fc33aa6a4514bc2653187ce2557
kamruzzamankajol1993 <kamruzzamankajol.cse@gmail.com>
2023-10-21 02:00:40 +0600

    database & controller created

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,41 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\Admin\BannerController;
+use App\Http\Controllers\Admin\ContactController;
+use App\Http\Controllers\Admin\DeliveryChargeController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\PackageComboController;
+use App\Http\Controllers\Admin\PrivacyPolicyController;
+use App\Http\Controllers\Admin\ProductController;
+
+use App\Http\Controllers\Admin\ReturnPolicyController;
+use App\Http\Controllers\Admin\ReviewController;
+use App\Http\Controllers\Admin\SocialLinkController;
+use App\Http\Controllers\Admin\SystemController;
+
+Route::controller(SystemController::class)->group(function () {
+
+
+});
+
+
+Route::group(['prefix' => 'admin'], function () {
+
+Route::resource('bannerList', BannerController::class);
+Route::resource('contactInfoList', ContactController::class);
+Route::resource('deliveryChargeList', DeliveryChargeController::class);
+Route::resource('orderList', OrderController::class);
+Route::resource('packageCombo', PackageComboController::class);
+Route::resource('privacyPolicy', PrivacyPolicyController::class);
+Route::resource('productList', ProductController::class);
+Route::resource('returnPolicy', ReturnPolicyController::class);
+Route::resource('reviewList', ReviewController::class);
+Route::resource('socialLinkList', SocialLinkController::class);
+Route::resource('systemInfo', SystemController::class);
+
+});
+
 
 Route::get('/', function () {
     return view('welcome');
